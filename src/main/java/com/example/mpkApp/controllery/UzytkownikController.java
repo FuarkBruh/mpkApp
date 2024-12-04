e3b55f849056302aaaddc071eeaeb88b09e4e1e2
Kamil <k.dorszewski.00@gmail.com>
2024-12-04 22:06:54 +0100

    requestMapping

--- src/main/java/com/example/mpkApp/controllery/UzytkownikController.java
+++ src/main/java/com/example/mpkApp/controllery/UzytkownikController.java
@@ -1,7 +1,9 @@
 package com.example.mpkApp.controllery;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/uzytkownik")
 public class UzytkownikController {
 }
