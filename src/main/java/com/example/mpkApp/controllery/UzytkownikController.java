74a5a245b40141db811d88e76354bd873c8c9304
Kamil <k.dorszewski.00@gmail.com>
2024-12-17 15:28:59 +0100

    Rozklad uzytkownik, controller wykonany

--- src/main/java/com/example/mpkApp/controllery/UzytkownikController.java
+++ src/main/java/com/example/mpkApp/controllery/UzytkownikController.java
@@ -1,9 +1,32 @@
 package com.example.mpkApp.controllery;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.mpkApp.modele.UzytkownikModel;
+import com.example.mpkApp.serwisy.UzytkownikSerwis;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/uzytkownik")
 public class UzytkownikController {
+    private final UzytkownikSerwis uzytkownikSerwis;
+
+    @Autowired
+    UzytkownikController(UzytkownikSerwis uzytkownikSerwis) {
+        this.uzytkownikSerwis = uzytkownikSerwis;
+    }
+
+    @PostMapping
+    public void newUzytkownik(UzytkownikModel uzytkownik) {
+        uzytkownikSerwis.newUzytkownik(uzytkownik);
+    }
+
+    @PutMapping("/{id}")
+    public void updatedUzytkownik(@PathVariable int id, UzytkownikModel uzytkownik) {
+        uzytkownikSerwis.updateUzytkownik(id, uzytkownik);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUzytkownik(@PathVariable int id) {
+        uzytkownikSerwis.deleteUzytkownik(id);
+    }
 }
--- src/main/java/com/example/mpkApp/serwisy/UzytkownikSerwis.java
+++ src/main/java/com/example/mpkApp/serwisy/UzytkownikSerwis.java
@@ -1,8 +1,12 @@
 package com.example.mpkApp.serwisy;
 
+import com.example.mpkApp.modele.UzytkownikModel;
 import com.example.mpkApp.repozytoria.UzytkownikRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class UzytkownikSerwis {
@@ -11,6 +15,29 @@ public class UzytkownikSerwis {
     @Autowired
     UzytkownikSerwis(UzytkownikRepo uzytkownikRepo) {
         this.uzytkownikRepo = uzytkownikRepo;
+    }
+
+    @Transactional
+    public void newUzytkownik(UzytkownikModel uzytkownik) {
+        uzytkownikRepo.save(uzytkownik);
+    }
+
+    @Transactional
+    public void updateUzytkownik(Integer id, UzytkownikModel UpdatedUzytkownik) {
+        Optional<UzytkownikModel> optionalUzytkowik = uzytkownikRepo.findById(id);
+        if (optionalUzytkowik.isEmpty()) {
+            throw new RuntimeException("Uzytkownik id " + id + " not found");
+        }
+        UzytkownikModel uzytkownik = optionalUzytkowik.get();
+        uzytkownik.setEmail(UpdatedUzytkownik.getEmail());
+        uzytkownik.setHaslo(UpdatedUzytkownik.getHaslo());
+        uzytkownik.setRola(UpdatedUzytkownik.getRola());
+        uzytkownikRepo.save(uzytkownik);
+    }
+
+    @Transactional
+    public void deleteUzytkownik(Integer id) {
+        uzytkownikRepo.deleteById(id);
     }
 
     @Override
